4a4ce082d4a26815b0c11ecc75175e0c7f1286d2
displayflex <3tonishere@gmail.com>
2017-12-25 15:03:07 +0300

    Написал Javascript модального окна карты, изменил карту

--- app/js/map-popup.js
+++ app/js/map-popup.js
@@ -1,1 +1,27 @@
-// 8: 2:10
+var mapOpen = document.querySelector(".js-open-map");
+// var mapOpenFooter = document.querySelector(".footer-contacts__link");
+var mapPopup = document.querySelector(".modal-content-map");
+var mapClose = document.querySelector(".modal-map__close");
+
+mapOpen.addEventListener('click', function(event) {
+	event.preventDefault();
+	mapPopup.classList.add("modal-map-show");
+});
+
+// mapOpenFooter.addEventListener('click', function(event) {
+// 	event.preventDefault();
+// 	mapPopup.classList.add("modal-map-show");
+// });
+
+mapClose.addEventListener('click', function(event) {
+	event.preventDefault();
+	mapPopup.classList.remove("modal-map-show");
+});
+
+window.addEventListener('keydown', function() {
+	if (event.keyCode === 27) {
+		if (mapPopup.classList.contains("modal-map-show")) {
+			mapPopup.classList.remove("modal-map-show");
+		}
+	}
+});
